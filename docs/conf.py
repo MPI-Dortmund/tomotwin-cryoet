ebd5d526128db9a23883bdc8d73f7af172d6345c
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2022-09-28 11:55:20 +0200

    fix logo path

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = 'img/logo_grey.png'
+html_logo = 'img/general/logo_grey.png'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
