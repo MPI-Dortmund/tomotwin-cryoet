9b452e7b1cd25dd67760a24b7528db200e921b72
thorstenwagner <twa1@posteo.de>
2022-12-15 10:05:34 +0100

    formatting

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='tomotwin-cryoet',
     use_scm_version={'write_to': 'tomotwin/__init__.py'},
-
     setup_requires=['setuptools_scm'],
     python_requires='>=3.7.0',
     packages=[
