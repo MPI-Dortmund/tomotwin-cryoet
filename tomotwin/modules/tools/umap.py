c8118eb4641a1488415a8f65fc62de69eef76009
thorstenwagner <twa1@posteo.de>
2022-12-22 08:23:39 +0100

    add optional to use a previously fitted model

--- tomotwin/modules/tools/umap.py
+++ tomotwin/modules/tools/umap.py
@@ -1,10 +1,12 @@
 import argparse
+import typing
 from argparse import ArgumentParser
 from tomotwin.modules.tools.tomotwintool import TomoTwinTool
 import pandas as pd
 import numpy as np
 import cuml
 from tqdm import tqdm
+import pickle
 
 from numpy.typing import ArrayLike
 class UmapTool(TomoTwinTool):
@@ -29,6 +31,8 @@ class UmapTool(TomoTwinTool):
 
         parser.add_argument('-o', '--output', type=str, required=True,
                             help='Output folder')
+        parser.add_argument('-m', '--model', type=str, required=False, default=None,
+                            help='Previously fitted model')
         parser.add_argument('--fit_sample_size', type=int, default=400000,
                             help='Sample size using for the fit of the umap')
 
@@ -40,22 +44,25 @@ class UmapTool(TomoTwinTool):
     def calcuate_umap(
             self, embeddings : pd.DataFrame,
             fit_sample_size: int,
-            transform_chunk_size: int) -> ArrayLike:
+            transform_chunk_size: int,
+            reducer: cuml.UMAP = None) -> typing.Tuple[ArrayLike, cuml.UMAP]:
         print("Prepare data")
 
         fit_sample = embeddings.sample(n=min(len(embeddings),fit_sample_size), random_state=17)
         fit_sample = fit_sample.drop(['filepath', 'Z', 'Y', 'X'], axis=1, errors='ignore')
         all_data = embeddings.drop(['filepath', 'Z', 'Y', 'X'],axis=1, errors='ignore')
-
-        reducer = cuml.UMAP(
-            n_neighbors=200,
-            n_components=2,
-            n_epochs=None,  # means automatic selection
-            min_dist=0.0,
-            random_state=19
-        )
-        print(f"Fit umap on {len(fit_sample)} samples")
-        reducer.fit(fit_sample)
+        if reducer is None:
+            reducer = cuml.UMAP(
+                n_neighbors=200,
+                n_components=2,
+                n_epochs=None,  # means automatic selection
+                min_dist=0.0,
+                random_state=19
+            )
+            print(f"Fit umap on {len(fit_sample)} samples")
+            reducer.fit(fit_sample)
+        else:
+            print("Use provided model. Don't fit.")
 
         num_chunks = max(1, int(len(all_data) / transform_chunk_size))
         print(f"Transform complete dataset in {num_chunks} chunks with a chunksize of ~{int(len(all_data)/num_chunks)}")
@@ -67,15 +74,23 @@ class UmapTool(TomoTwinTool):
 
         embedding = np.concatenate(chunk_embeddings)
 
-        return embedding
+        return embedding, reducer
 
 
     def run(self, args):
         print("Read data")
         embeddings = pd.read_pickle(args.input)
         out_pth = args.output
-        umap_embeddings = self.calcuate_umap(embeddings=embeddings, fit_sample_size=args.fit_sample_size, transform_chunk_size=args.chunk_size)
+        model = None
+        if args.model:
+            model = pickle.load(open(args.model, "rb"))
+        umap_embeddings, fitted_umap = self.calcuate_umap(embeddings=embeddings,
+                                                          fit_sample_size=args.fit_sample_size,
+                                                          transform_chunk_size=args.chunk_size,
+                                                          reducer=model)
         import os
         os.makedirs(out_pth,exist_ok=True)
 
         pd.DataFrame(umap_embeddings).to_pickle(os.path.join(out_pth,os.path.splitext(os.path.basename(args.input))[0]+".tumap"))
+        print("Pickle model")
+        pickle.dump(fitted_umap, open(os.path.join(out_pth,"fitted_model.pkl"), "wb"))
