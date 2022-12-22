82a00e359afdf4979a8f4edb2607ee906b16f804
thorstenwagner <twa1@posteo.de>
2022-12-22 10:47:38 +0100

    make n_neighbors a parameter

--- tomotwin/modules/tools/umap.py
+++ tomotwin/modules/tools/umap.py
@@ -7,6 +7,7 @@ import numpy as np
 import cuml
 from tqdm import tqdm
 import pickle
+import os
 
 from numpy.typing import ArrayLike
 class UmapTool(TomoTwinTool):
@@ -33,6 +34,8 @@ class UmapTool(TomoTwinTool):
                             help='Output folder')
         parser.add_argument('-m', '--model', type=str, required=False, default=None,
                             help='Previously fitted model')
+        parser.add_argument('--neighbors', type=int, required=False, default=200,
+                            help='Previously fitted model')
         parser.add_argument('--fit_sample_size', type=int, default=400000,
                             help='Sample size using for the fit of the umap')
 
@@ -45,7 +48,8 @@ class UmapTool(TomoTwinTool):
             self, embeddings : pd.DataFrame,
             fit_sample_size: int,
             transform_chunk_size: int,
-            reducer: cuml.UMAP = None) -> typing.Tuple[ArrayLike, cuml.UMAP]:
+            reducer: cuml.UMAP = None,
+            neighbors: int = 200) -> typing.Tuple[ArrayLike, cuml.UMAP]:
         print("Prepare data")
 
         fit_sample = embeddings.sample(n=min(len(embeddings),fit_sample_size), random_state=17)
@@ -53,7 +57,7 @@ class UmapTool(TomoTwinTool):
         all_data = embeddings.drop(['filepath', 'Z', 'Y', 'X'],axis=1, errors='ignore')
         if reducer is None:
             reducer = cuml.UMAP(
-                n_neighbors=200,
+                n_neighbors=neighbors,
                 n_components=2,
                 n_epochs=None,  # means automatic selection
                 min_dist=0.0,
@@ -87,10 +91,10 @@ class UmapTool(TomoTwinTool):
         umap_embeddings, fitted_umap = self.calcuate_umap(embeddings=embeddings,
                                                           fit_sample_size=args.fit_sample_size,
                                                           transform_chunk_size=args.chunk_size,
-                                                          reducer=model)
-        import os
-        os.makedirs(out_pth,exist_ok=True)
+                                                          reducer=model,
+                                                          neighbors=args.neighbors)
 
-        pd.DataFrame(umap_embeddings).to_pickle(os.path.join(out_pth,os.path.splitext(os.path.basename(args.input))[0]+".tumap"))
-        print("Pickle model")
-        pickle.dump(fitted_umap, open(os.path.join(out_pth,"fitted_model.pkl"), "wb"))
+        os.makedirs(out_pth,exist_ok=True)
+        fname = os.path.splitext(os.path.basename(args.input))[0]
+        pd.DataFrame(umap_embeddings).to_pickle(os.path.join(out_pth,fname+".tumap"))
+        pickle.dump(fitted_umap, open(os.path.join(out_pth,fname+"_umap_model.pkl"), "wb"))
