b91f3f713c86a828b71e9104f5e0491786de320d
thorstenwagner <twa1@posteo.de>
2023-03-28 11:53:05 +0200

    add test for main function

--- tests/test_embed_main.py
+++ tests/test_embed_main.py
@@ -1,11 +1,14 @@
 import os.path
 import unittest
+
+import tomotwin.embed_main
 from tomotwin.modules.inference.embedor import Embedor
 import numpy as np
-from tomotwin.modules.inference.argparse_embed_ui import EmbedConfiguration
+from tomotwin.modules.inference.argparse_embed_ui import EmbedConfiguration, EmbedMode
 import tempfile
 from tomotwin.modules.inference.volumedata import VolumeDataset
 import mrcfile
+from unittest.mock import MagicMock
 
 class DummyEmbedor(Embedor):
 
@@ -17,6 +20,29 @@ class DummyEmbedor(Embedor):
         return embeddings
 
 class TestsEmbedMain(unittest.TestCase):
+
+    def test_embed_main(self):
+        from tomotwin.embed_main import _main_ as embed_main_func
+        tomo = np.random.randn(50, 50, 50).astype(np.float32)
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            volume_path = os.path.join(tmpdirname, "vola.mrc")
+
+            with mrcfile.new(volume_path) as mrc:
+                mrc.set_data(tomo)
+            tomotwin.embed_main.make_embeddor = MagicMock(return_value=DummyEmbedor())
+            tomotwin.embed_main.get_window_size = MagicMock(return_value=37)
+            embed_conf = EmbedConfiguration(
+                model_path=None,
+                volumes_path=volume_path,
+                output_path=tmpdirname,
+                mode=EmbedMode.TOMO,
+                batchsize=3,
+                stride=1,
+                zrange=None
+            )
+            embed_main_func(embed_conf)
+            self.assertEqual(True, os.path.exists(os.path.join(tmpdirname, "vola_embeddings.temb")))
 
     def test_embed_tomogram(self):
         from tomotwin.embed_main import embed_tomogram
