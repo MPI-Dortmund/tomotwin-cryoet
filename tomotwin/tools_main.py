712c54f6a9fddd91f04e0e389b823b4725e07119
thorstenwagner <twa1@posteo.de>
2023-05-10 15:25:35 +0200

    some refactoring

--- tomotwin/tools_main.py
+++ tomotwin/tools_main.py
@@ -379,38 +379,33 @@ import argparse
 import sys
 from typing import List
 
+from tomotwin.modules.tools.embedding_mask import EmbeddingMaskTool
+from tomotwin.modules.tools.extract_reference import ExtractReference
+from tomotwin.modules.tools.filter_embedding import FilterTool
+from tomotwin.modules.tools.info import Info
+from tomotwin.modules.tools.make_target import MakeTargetEmbeddings
+from tomotwin.modules.tools.median_embedding import MedianTool
+from tomotwin.modules.tools.scale_coordinates import ScaleCoordinates
 from tomotwin.modules.tools.tomotwintool import TomoTwinTool
+from tomotwin.modules.tools.umap import UmapTool
 
 
 def get_tool_list() -> List[TomoTwinTool]:
+    '''
+    Create the list of supported tools
+    :return: List of tools
+    '''
+
     tools = []
 
-    from tomotwin.modules.tools.extract_reference import ExtractReference
-    from tomotwin.modules.tools.scale_coordinates import ScaleCoordinates
-    from tomotwin.modules.tools.info import Info
-    from tomotwin.modules.tools.umap import UmapTool
-    from tomotwin.modules.tools.median_embedding import MedianTool
-    from tomotwin.modules.tools.filter_embedding import FilterTool
-    from tomotwin.modules.tools.embedding_mask import EmbeddingMaskTool
-    from tomotwin.modules.tools.make_target import MakeTargetEmbeddings
-
-    scale_coords_tool = ScaleCoordinates()
-    extract_ref_tool = ExtractReference()
-    umap_tool = UmapTool()
-    info_tool = Info()
-    median_tool = MedianTool()
-    fiter_tool = FilterTool()
-    embedding_mask_tool = EmbeddingMaskTool()
-    make_target_tool = MakeTargetEmbeddings()
-
-    tools.append(extract_ref_tool)
-    tools.append(scale_coords_tool)
-    tools.append(info_tool)
-    tools.append(umap_tool)
-    tools.append(median_tool)
-    tools.append(fiter_tool)
-    tools.append(embedding_mask_tool)
-    tools.append(make_target_tool)
+    tools.append(ScaleCoordinates())
+    tools.append(ExtractReference())
+    tools.append(UmapTool())
+    tools.append(Info())
+    tools.append(MedianTool())
+    tools.append(FilterTool())
+    tools.append(EmbeddingMaskTool())
+    tools.append(MakeTargetEmbeddings())
 
     return tools
 
