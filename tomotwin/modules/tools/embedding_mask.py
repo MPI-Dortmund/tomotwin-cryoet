ae3e1942a1c6b9d15f7e2a005885d04236e6f552
thorstenwagner <twa1@posteo.de>
2023-05-10 15:45:30 +0200

    black code

--- tomotwin/modules/tools/embedding_mask.py
+++ tomotwin/modules/tools/embedding_mask.py
@@ -388,60 +388,70 @@ from tomotwin.modules.tools.tomotwintool import TomoTwinTool
 
 
 class EmbeddingMaskTool(TomoTwinTool):
-    '''
+    """
     Tools to create mask with label IDs for tomogram embeddings
-    '''
+    """
 
     def get_command_name(self) -> str:
-        return 'embedding_mask'
+        return "embedding_mask"
 
     def create_parser(self, parentparser: ArgumentParser) -> ArgumentParser:
-        '''
+        """
         :param parentparser: ArgumentPaser where the subparser for this tool needs to be added.
         :return: Argument parser that was added to the parentparser
-        '''
+        """
 
         parser = parentparser.add_parser(
             self.get_command_name(),
             help="Generates an label mask for use in the clustering workflow with Napari",
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         )
-        parser.add_argument('-i', '--input', type=str, required=True,
-                            help='Embeddings file to use for clustering')
+        parser.add_argument(
+            "-i",
+            "--input",
+            type=str,
+            required=True,
+            help="Embeddings file to use for clustering",
+        )
 
-        parser.add_argument('-o', '--output', type=str, required=True,
-                            help='Output folder')
+        parser.add_argument(
+            "-o", "--output", type=str, required=True, help="Output folder"
+        )
 
         return parser
 
-    def create_embedding_mask(
-            self, embeddings: pd.DataFrame):
+    def create_embedding_mask(self, embeddings: pd.DataFrame):
         """
         Creates mask where each individual subvolume of the running windows gets an individual ID
         """
         print("Create embedding mask")
-        Z = embeddings.attrs['tomogram_input_shape'][0]
-        Y = embeddings.attrs['tomogram_input_shape'][1]
-        X = embeddings.attrs['tomogram_input_shape'][2]
-        stride = embeddings.attrs['stride'][0]
+        Z = embeddings.attrs["tomogram_input_shape"][0]
+        Y = embeddings.attrs["tomogram_input_shape"][1]
+        X = embeddings.attrs["tomogram_input_shape"][2]
+        stride = embeddings.attrs["stride"][0]
         embeddings = embeddings.reset_index(drop=True)
-        segmentation_mask = embeddings[['Z', 'Y', 'X']].copy()
+        segmentation_mask = embeddings[["Z", "Y", "X"]].copy()
         segmentation_mask = segmentation_mask.reset_index()
         empty_array = np.zeros(shape=(Z, Y, X))
-        for row in tqdm(segmentation_mask.itertuples(index=True, name='Pandas'), total=len(segmentation_mask)):
+        for row in tqdm(
+                segmentation_mask.itertuples(index=True, name="Pandas"),
+                total=len(segmentation_mask),
+        ):
             X = int(row.X)
             Y = int(row.Y)
             Z = int(row.Z)
             label = int(row.index)
-            empty_array[(Z):(Z + stride), (Y):(Y + stride), (X):(X + stride)] = label + 1
+            empty_array[(Z): (Z + stride), (Y): (Y + stride), (X): (X + stride)] = (
+                    label + 1
+            )
         segmentation_array = empty_array.astype(np.float32)
 
         return segmentation_array
 
     def run(self, args):
-        '''
+        """
         Runs the tools
-        '''
+        """
         print("Read data")
         embeddings = pd.read_pickle(args.input)
         print("Generate label mask")
@@ -449,13 +459,11 @@ class EmbeddingMaskTool(TomoTwinTool):
         print("Write results to disk")
         os.makedirs(args.output, exist_ok=True)
         with mrcfile.new(
-                os.path.join(args.output, os.path.splitext(os.path.basename(args.input))[0] + "_label_mask.mrci"),
-                overwrite=True) as mrc:
+                os.path.join(
+                    args.output,
+                    os.path.splitext(os.path.basename(args.input))[0] + "_label_mask.mrci",
+                ),
+                overwrite=True,
+        ) as mrc:
             mrc.set_data(segmentation_layer)
         print("Done")
-
-
-
-
-
-
