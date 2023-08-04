2893d6ec67da7f5aa13d0b944fbb5fcfc1cf1111
thorstenwagner <twa1@posteo.de>
2023-08-04 09:52:39 +0200

    some pep8 changes

--- tomotwin/modules/inference/embed_ui.py
+++ tomotwin/modules/inference/embed_ui.py
@@ -359,7 +359,7 @@ Exhibit A - Source Code Form License Notice
 
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 If it is not possible or desirable to put the notice in a particular
 file, then You may include the notice in a location (such as a LICENSE
@@ -398,21 +398,15 @@ class DistrMode(Enum):
 class EmbedConfiguration:
     """
     Represents the configuration for embedding calculation
-
-    :param model_path: Path to the model
-    :param volumes: Path to folder with subvolumes, subvolumes itself or path to a tomogram
-    :param mode: EmbedMode to run
-    :param window_size: Running window size
-    :param stride: Stride for running window
     """
 
     model_path: str
-    volumes_path: List[str]
+    volumes_path: str | List[str]
     output_path: str
     mode: EmbedMode
     batchsize: int
     stride: int = None
-    zrange: Tuple[int,int] = None
+    zrange: Tuple[int, int] = None
     maskpth: str = None
     distr_mode: DistrMode = None
 
@@ -432,5 +426,5 @@ class EmbedUI(ABC):
     def get_embed_configuration(self) -> EmbedConfiguration:
         """
         Creates the embed configuration and returns it.
-        :return: A embed configuration instance
+        :return: An embed configuration instance
         """
