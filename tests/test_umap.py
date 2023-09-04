bf107a298b170132079957e6132812e470cdfda0
thorstenwagner <twa1@posteo.de>
2023-09-04 14:42:25 +0200

    add unit tests and add rapids 23.08 dep.

--- tests/test_umap.py
+++ tests/test_umap.py
@@ -3,9 +3,11 @@ import tempfile
 import unittest
 
 import pandas as pd
-from cuml.common.device_selection import using_device_type
 
 from tomotwin.modules.tools.umap import UmapTool
+
+
+# from cuml.common.device_selection import using_device_type
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,16 +17,16 @@ class MyTestCase(unittest.TestCase):
                               "../resources/tests/map_main/embed/tomo/tiltseries_cropped_embeddings.temb")
 
         with tempfile.TemporaryDirectory() as tmpdirname:
-            with using_device_type('cpu'):
-                dat = pd.read_pickle(in_pth)
-                pd.to_pickle(dat[:500], os.path.join(tmpdirname, "emb.temb"))
-                tool._run(input_pth=os.path.join(tmpdirname, "emb.temb"),
-                          out_pth=tmpdirname,
-                          fit_sample_size=500,
-                          fit_chunk_size=500,
-                          neighbors=200,
-                          ncomponents=2
-                          )
+            # with using_device_type('cpu'):
+            dat = pd.read_pickle(in_pth)
+            pd.to_pickle(dat[:500], os.path.join(tmpdirname, "emb.temb"))
+            tool._run(input_pth=os.path.join(tmpdirname, "emb.temb"),
+                      out_pth=tmpdirname,
+                      fit_sample_size=500,
+                      fit_chunk_size=500,
+                      neighbors=200,
+                      ncomponents=2
+                      )
 
             self.assertEqual(True,
                              os.path.exists(os.path.join(tmpdirname, "emb_label_mask.mrci")))  # add assertion here
