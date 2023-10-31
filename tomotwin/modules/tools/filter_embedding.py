0c71a7586c55d39739b27b4b00328470b2940523
thorstenwagner <twa1@posteo.de>
2023-10-31 11:26:48 +0100

    add new filtering options that allow the refinement of reference picks

--- tomotwin/modules/tools/filter_embedding.py
+++ tomotwin/modules/tools/filter_embedding.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 
+import numpy as np
 import pandas as pd
 
 from tomotwin.modules.tools.tomotwintool import TomoTwinTool
@@ -29,28 +30,57 @@ class FilterTool(TomoTwinTool):
                             help='Embeddings file')
 
         parser.add_argument('-m', '--map', type=str, required=True,
-                            help='Map file for median embedding')
+                            help='tmap file')
 
         parser.add_argument('-t', '--threshold', type=float, required=True,
-                            help='All embeddings higher than this similarty threshold are discarded')
+                            help='All embeddings higher (use --lower otherwise) than this similarty threshold are discarded')
 
         parser.add_argument('-o', '--output', type=str, required=True,
                             help='Output folder')
 
+        parser.add_argument('--lower',
+                            action='store_true',
+                            help="Discard embeddings lower than given threshold")
+
+        parser.add_argument('--concat',
+                            action='store_true',
+                            help="Concatenate all filtered embeddings into one embedding file")
+
     from typing import List
     @staticmethod
-    def filter_embeddings(embeddings: pd.DataFrame, map_results: pd.DataFrame, threshold: float) -> List[pd.DataFrame]:
+    def filter_embeddings(embeddings: pd.DataFrame, map_results: pd.DataFrame, threshold: float, discard_lower=False,
+                          concat=False) -> List[pd.DataFrame]:
         """
         Removes all embeddings with a similarity to the median embedding higher than the threshold. Runs filtering for
         each reference in map_results seperately
         """
+
+        def apply_thresh(tmap: pd.DataFrame, thresh: float, discard_lower: bool):
+            if discard_lower:
+                return tmap >= thresh
+            else:
+                return tmap < thresh
+
+
         filtered_embeddings = []
         map_results_no_coords = map_results.drop(['X', 'Y', 'Z'], axis=1)
+        masks = []
         for ref_index, reference in enumerate(map_results_no_coords):
             print(f"Filter reference {map_results_no_coords.attrs['references'][ref_index]} (Map column: {reference})")
-            mask = map_results_no_coords[reference] < threshold
+
+            mask = apply_thresh(map_results_no_coords[reference], threshold, discard_lower)
             mask = mask.to_numpy()
-            filtered_embeddings.append(embeddings.iloc[mask])
+
+            if not concat:
+                filtered_embeddings.append(embeddings.iloc[mask])
+            else:
+                masks.append(mask)
+
+        if concat:
+            m = masks[0]
+            for i in range(1, len(masks)):
+                m = np.logical_or(m, masks[i])
+            filtered_embeddings.append(embeddings.iloc[m])
 
         return filtered_embeddings
 
@@ -61,10 +91,18 @@ class FilterTool(TomoTwinTool):
 
         filtered = self.filter_embeddings(embeddings=tomo_embeddings,
                                           map_results=tomo_map,
-                                          threshold=args.threshold)
+                                          threshold=args.threshold,
+                                          discard_lower=args.lower,
+                                          concat=args.concat)
         embedding_filename = os.path.splitext(os.path.basename(args.input))[0]
-        for emb_index, emb in enumerate(filtered):
-            ref_name = os.path.splitext(os.path.basename(tomo_map.attrs['references'][emb_index]))[0]
-            out=os.path.join(args.output,f"{embedding_filename}_filtered_{ref_name}.temb")
+        if not args.concat:
+            for emb_index, emb in enumerate(filtered):
+                ref_name = os.path.splitext(os.path.basename(tomo_map.attrs['references'][emb_index]))[0]
+                out = os.path.join(args.output, f"{embedding_filename}_filtered_{ref_name}.temb")
+                emb.to_pickle(out)
+                print(f"Wrote {out} - removed {100 - len(emb) / len(tomo_embeddings) * 100:.2f}% embedding points")
+        else:
+            emb = filtered[0]
+            out = os.path.join(args.output, f"{embedding_filename}_filtered_allrefs.temb")
             emb.to_pickle(out)
-            print(f"Wrote {out} - removed {100 - len(emb)/len(tomo_embeddings)*100:.2f}% embedding points")
+            print(f"Wrote {out} - removed {100 - len(emb) / len(tomo_embeddings) * 100:.2f}% embedding points")
