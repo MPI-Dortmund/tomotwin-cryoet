eef49336c04d4e6210b3a5c07a56270453371543
thorstenwagner <twa1@posteo.de>
2023-11-07 08:58:50 +0100

    umap unittest is now working. Skip if no gpu is available.

--- tests/test_umap.py
+++ tests/test_umap.py
@@ -3,6 +3,8 @@ import tempfile
 import unittest
 
 import pandas as pd
+import pytest
+import torch
 
 from tomotwin.modules.tools.umap import UmapTool
 
@@ -11,22 +13,30 @@ from tomotwin.modules.tools.umap import UmapTool
 
 
 class MyTestCase(unittest.TestCase):
+
+    @pytest.mark.skipif(torch.cuda.is_available() == False, reason="Skipped because CUDA is not available")
     def test_something(self):
         tool = UmapTool()
         in_pth = os.path.join(os.path.dirname(__file__),
                               "../resources/tests/map_main/embed/tomo/tiltseries_cropped_embeddings.temb")
+        from types import SimpleNamespace
 
         with tempfile.TemporaryDirectory() as tmpdirname:
             # with using_device_type('cpu'):
             dat = pd.read_pickle(in_pth)
             pd.to_pickle(dat[:500], os.path.join(tmpdirname, "emb.temb"))
-            tool._run(input_pth=os.path.join(tmpdirname, "emb.temb"),
-                      out_pth=tmpdirname,
-                      fit_sample_size=500,
-                      fit_chunk_size=500,
-                      neighbors=200,
-                      ncomponents=2
-                      )
+            args = {
+                "input": os.path.join(tmpdirname, "emb.temb"),
+                "fit_sample_size": 500,
+                "chunk_size": 500,
+                "neighbors": 200,
+                "ncomponents": 2,
+                "output": tmpdirname,
+                "model": None,
+                "cosine": None,
+            }
+
+            tool.run(SimpleNamespace(**args))
 
             self.assertEqual(True,
                              os.path.exists(os.path.join(tmpdirname, "emb_label_mask.mrci")))  # add assertion here
